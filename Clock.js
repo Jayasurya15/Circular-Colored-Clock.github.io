d82a14cab0c06b83b94dff0875188edb6381b45d
Jayasurya15 <74535089+Jayasurya15@users.noreply.github.com>
2025-04-26 22:44:41 +0530

    Update Clock.js

--- Clock.js
+++ Clock.js
@@ -3,14 +3,14 @@ const circle = [new Circle('minutes', '#ff0000'), new Circle('seconds', '#f37e21
 function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-    // Draw Clock Border
+    // Clock Border
     BorderClock(document.getElementById('clock').value);
 
-    // Draw Circles
+    // Circles
     circle[0].draw(document.getElementById('outside').value);
     circle[1].draw(document.getElementById('inside').value);
 
-    // Draw Text Clock
+    // Text Clock
     TextClock();
 
     // Loop
